595ea88d89ce564b763aebcba64422cd739a5f92
ChaRam05 <charmainemae.ramirez@cit.edu>
2023-05-23 15:22:06 +0800

    Payment Routes Updated

--- Client/distributorsystem/src/Global Components/Routes.tsx
+++ Client/distributorsystem/src/Global Components/Routes.tsx
@@ -15,6 +15,7 @@ import DistributorOrderForm from "../Components/Product Distribution/Distributor
 import DealerOrderForm from "../Components/Product Distribution/DealerOrderForm";
 import DealaerProfile from "../Components/Profiles/DealerProfile";
 import SignIn from "../Components/Sign-in/SignInScreen";
+import RecordDirectPayment from "../Components/Payments/RecordDirectPayment";
 export default function MainRoutes(){
     return(
         <Routes>
@@ -23,6 +24,8 @@ export default function MainRoutes(){
                 <Route path="/dashboard" element={<Dashboard/>}/>
                 <Route path="/assignmentCollector" element={<AssignmentList/>}/>
                 <Route path="/paymentList" element={<PaymentList/>}/>
+                <Route path="/paymentSummary" element={<PaymentSummary/>}/>
+                <Route path="/recordDirectPayment" element={<RecordDirectPayment/>}/>
                 <Route path="/dealearOrderForm" element={<DealerOrderForm/>}/>
                 <Route path="/distributorOrderForm" element={<DistributorOrderForm/>}/>
                 {/* <Route path="/orderConfirmation" element={<orderConfirmation/>}/> */}
