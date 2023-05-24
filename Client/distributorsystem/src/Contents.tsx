d69f9f0c3ff44ad4ac8b3d56e7c539db1df5845a
ChaRam05 <charmainemae.ramirez@cit.edu>
2023-05-24 14:13:55 +0800

    Minor error

--- Client/distributorsystem/src/Contents.tsx
+++ Client/distributorsystem/src/Contents.tsx
@@ -11,6 +11,10 @@ export default function Content() {
         '/dashboard':'Dashboard',
         '/assignmentCollector':'Assignment Collector',
         '/dealerRegistration':'Dealer Registration',
+
+        '/distributorOrderForm':'Distribution Order Form',
+        '/orderConfirmation': 'Order Confrimation',
+
         '/paymentList':'Payment List',
         '/paymentSummary':'Payment Summary',
         '/recordDirectPayment':'Record Direct Payment',
