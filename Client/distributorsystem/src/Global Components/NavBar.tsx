b7a1355eeecaa822e3b97d51f90da2a4275f0832
kemrielnickperez <Kemrielnick.perez@cit.edu>
2023-05-27 01:18:36 +0800

    Here is the updated code of mah collector assignment and stylized navbar, please check your routings and adjust it lng fo if naay something wrong thanks

--- Client/distributorsystem/src/Global Components/NavBar.tsx
+++ Client/distributorsystem/src/Global Components/NavBar.tsx
@@ -10,66 +10,80 @@ import DealerRegistration from '../Components/Registration/DealerRegistration';
 import ScheduleOrderTransaction from '../Components/Schedules/ScheduleOrderTransaction';
 import AssignmentList from '../Components/Collector Assignment/AssignmentList';
 
-type navProps = {
-  moduleName: string;
-}
+  type navProps = {
+    moduleName: string;
+  }
+//** Function here*/
+        export default function NavBar(props: navProps) {
+          const [isOpen, setOpen] = useState(false); // State for opening navbar
+          const location = useLocation(); // State for locating route's path
+          //const navigate = useNavigate();
 
-export default function NavBar(props: navProps) {
-  const [isOpen, setOpen] = useState(false);
-  const location = useLocation();
-  const navigate = useNavigate();
+    //Closing Navbar when switching to another page
+        useEffect(() => {
+          setOpen(false);
+        }, 
+        [location]);
 
-  useEffect(() => {
-    setOpen(false);
-  }, [location]);
+        //** Return Statement*/  
+              return (
+                // Box for Header Nav
+                  <Box sx={{ flexGrow: 1, bgcolor: "#156D94" }}>
+                    <AppBar position="static" sx={{ bgcolor: "#156D94" }}>
+                        <Toolbar>
+                          {/* Icon Button for Menu Icon */}
+                            <IconButton
+                              size="large"
+                              edge="start"
+                              color="inherit"
+                              aria-label="menu"
+                              sx={{ mr: 2 }}
+                              onClick={() => setOpen(true)}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                          {/* Typography for Nav Title Module name/ Page name */}
+                            <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'left', fontWeight: "bold" }}>
+                                {props.moduleName}
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                          {/* Sidebar Drawer */}
+                    <Drawer anchor='left' open={isOpen} onClose={() => setOpen(false)} >
+                      <Box sx={{ backgroundColor: "#146C94", width: '400px', display: 'flex', flexDirection: 'column', height: '100%' }} role='presentation'>
+                        <Box p={2} display="flex" alignItems="center" justifyContent="space-between">
 
-  return (
-    <Box sx={{ flexGrow: 1, bgcolor: "#156D94" }}>
-      <AppBar position="static" sx={{ bgcolor: "#156D94" }}>
-        <Toolbar  >
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={() => setOpen(true)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'left', fontWeight: "bold" }}>
-            {props.moduleName}
-          </Typography>
-        </Toolbar>
-      </AppBar>
+                        {/* Typography for Menu Title Module name or Page name */} 
+                              <Typography variant='h6' component='div' sx={{ fontSize:'20px',fontWeight: "bold", color: "#FFFFFF",  marginLeft: 'auto' }}>
+                                {props.moduleName}
+                              </Typography>
 
-      <Drawer anchor='left' open={isOpen} onClose={() => setOpen(false)} >
-        <Box sx={{ backgroundColor: "#146C94", width: '400px', display: 'flex', flexDirection: 'column', height: '100%' }} role='presentation'>
-          <Box p={2} display="flex" alignItems="center" justifyContent="space-between">
-            <Typography variant='h6' component='div' sx={{ fontSize:'20px',fontWeight: "bold", color: "#FFFFFF",  marginLeft: 'auto' }}>
-              {props.moduleName}
-            </Typography>
-            {/* <Box p={2} textAlign="center"> */}
-            <IconButton
-              size="large"
-              edge="end"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mx: 'auto', alignItems:'right' }}
-              onClick={() => setOpen(false)}
-            >
-              <ArrowBackIcon sx={{color:'white'}}/>
-            </IconButton>
-          {/* </Box> */}
-          </Box>
-          <Box sx={{ flexGrow: 1,  flexDirection: 'column', justifyContent: 'center' }}>
-          <Link to="/dashboard"><Button sx={{backgroundColor: 'rgba(19, 71, 95, 0.1)' ,fontWeight:'bold',color:'#FFFFFF', height:'80px',width:'100%','&:hover': { backgroundColor: '#13475F' }}}>Dashboard</Button></Link>
-          <Link to="/dealerRegistration"><Button sx={{ backgroundColor: 'rgba(19, 71, 95, 0.1)' ,fontWeight:'bold',color:'#FFFFFF', height:'80px',width:'100%','&:hover': { backgroundColor: '#13475F' }}}>Dealer Registration</Button></Link>
-          <Link to="/assignmentCollector"><Button sx={{backgroundColor: 'rgba(19, 71, 95, 0.1)' ,fontWeight:'bold',color:'#FFFFFF', height:'80px',width:'100%','&:hover': { backgroundColor: '#13475F' }}}>Assignment Collector</Button></Link>
-          <Link to="/distributorOrderForm"><Button sx={{backgroundColor: 'rgba(19, 71, 95, 0.1)' ,fontWeight:'bold',color:'#FFFFFF', height:'80px',width:'100%','&:hover': { backgroundColor: '#13475F'  }}}>Distributor Order Form</Button></Link>
-          </Box>
-        </Box>
-      </Drawer>
-    </Box>
-  )
-}
+                        {/* Icon Button for Arrow Back to Close sidebar */}
+                              <IconButton
+                                size="large"
+                                edge="end"
+                                color="inherit"
+                                aria-label="menu"
+                                sx={{ mx: 'auto', alignItems:'right' }}
+                                onClick={() => setOpen(false)}
+                              >
+                                <ArrowBackIcon sx={{color:'white'}}/>
+                              </IconButton>
+                        </Box>
+
+                        {/* Route Here for your Page */}
+                        {/*Box for Pages*/}
+                        <Box sx={{ flexGrow: 1,  flexDirection: 'column', justifyContent: 'center' }}>
+                              <Link to="/dashboard"><Button sx={{backgroundColor: 'rgba(19, 71, 95, 0.1)' ,fontWeight:'bold',color:'#FFFFFF', height:'80px',width:'100%','&:hover': { backgroundColor: '#13475F' }}}>Dashboard</Button></Link>
+                            
+                              <Link to="/dealerRegistration"><Button sx={{ backgroundColor: 'rgba(19, 71, 95, 0.1)' ,fontWeight:'bold',color:'#FFFFFF', height:'80px',width:'100%','&:hover': { backgroundColor: '#13475F' }}}>Dealer Registration</Button></Link>
+                            
+                              <Link to="/assignmentCollector"><Button sx={{backgroundColor: 'rgba(19, 71, 95, 0.1)' ,fontWeight:'bold',color:'#FFFFFF', height:'80px',width:'100%','&:hover': { backgroundColor: '#13475F' }}}>Collector Assignment</Button></Link>
+                            
+                              <Link to="/distributorOrderForm"><Button sx={{backgroundColor: 'rgba(19, 71, 95, 0.1)' ,fontWeight:'bold',color:'#FFFFFF', height:'80px',width:'100%','&:hover': { backgroundColor: '#13475F'  }}}>Distributor Order Form</Button></Link>
+                        </Box>
+                      </Box>
+                    </Drawer>
+                  </Box>
+                )
+        }
