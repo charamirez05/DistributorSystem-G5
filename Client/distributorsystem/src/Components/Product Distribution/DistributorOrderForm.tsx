30698597db50585db60d9f70b9c1d21c96f4cc35
JihyoSan17 <aerismayhem17@gmail.com>
2023-05-26 23:05:52 +0800

    Added Payments Changes

--- Client/distributorsystem/src/Components/Product Distribution/DistributorOrderForm.tsx
+++ Client/distributorsystem/src/Components/Product Distribution/DistributorOrderForm.tsx
@@ -34,7 +34,6 @@ export default function distributorOrderForm(){
     return( 
         <>
         <div>
-                <h1>sadaws</h1>
         <NavBar moduleName=''/>
          <div style={{marginLeft:"50px", marginTop:"20px",marginRight:"50px", marginBottom:"20px"}}>
          <h1 style={{color:"White", fontFamily:"Verdana"}}>Product Distribution Form</h1>
